51899feeb03975c8144e2221f0e50a89e998fb98
Enzo Mularczyk <enzomularczyk@hotmail.fr>
2022-10-08 17:19:14 +0200

    changement du inner.html

--- front/js/product.js
+++ front/js/product.js
@@ -26,23 +26,48 @@ function getProduct(ProductId) {
 
 }
 
+//function displayProduct(Product) {
+//    document.getElementsByClassName("item__img")[0].innerHTML = `
+//    <img src="${Product.imageUrl}" alt="${Product.altTxt}">
+//    `
+//    document.getElementById("title").innerHTML =`
+//    ${Product.name}
+//    `
+//    document.getElementById("price").innerHTML =`
+//    ${Product.price}
+//   `
+//    document.getElementById("description").innerHTML =`
+//    ${Product.description}
+//    `
+//    for (let i = 0; i < Product.colors.length; i++){
+//    const color = Product.colors[i]
+//    document.getElementById("colors").innerHTML +=`
+//    <option value="color">${Product.colors[i]}</option>
+//    `
+//    }
+//}
+
 function displayProduct(Product) {
-    document.getElementsByClassName("item__img")[0].innerHTML = `
-    <img src="${Product.imageUrl}" alt="${Product.altTxt}">
-    `
-    document.getElementById("title").innerHTML =`
-    ${Product.name}
-    `
-    document.getElementById("price").innerHTML =`
-    ${Product.price}
-    `
-    document.getElementById("description").innerHTML =`
-    ${Product.description}
-    `
-    for (let i = 0; i < Product.colors.length; i++){
-    const color = Product.colors[i]
-    document.getElementById("colors").innerHTML +=`
-    <option value="color">${Product.colors[i]}</option>
-    `
+
+    const elt_img = document.createElement("img")
+    elt_img.setAttribute("src", Product.imageUrl)
+    elt_img.setAttribute("alt", Product.altTxt)
+
+    document.getElementsByClassName("item__img")[0].appendChild(elt_img)
+
+    const elt_h1 =  document.getElementById('title')
+    elt_h1.textContent = Product.name
+
+    const elt_description = document.getElementById("description")
+    elt_description.textContent = Product.description
+
+    const elt_select =  document.getElementById('colors')
+
+    for (let color in Product.colors){
+        const elt_option = document.createElement("option")
+        elt_option.setAttribute("value", Product.colors[color])
+        elt_option.textContent = Product.colors[color]
+        elt_select.appendChild(elt_option)
     }
+
 }
--- front/js/script.js
+++ front/js/script.js
@@ -3,32 +3,54 @@ main ()
 async function main(){
     const Products = await getProducts()
     console.log(Products)
-    for (let i = 0; i < Products.length; i++) {
-        const Product = Products[i]
+    for (let Product of Products) {
         displayProducts(Product)
     }
 }
 
-function getProducts() {
-    return fetch("http://localhost:3000/api/products")
-        .then(function(Response){
-            return Response.json()
-        })
-        .then(function(Products){
-            return Products
-        })
-        .catch(function(error) {
-            alert(error)
-        })
+async function getProducts() {
+    try {
+        const Response = await fetch("http://localhost:3000/api/products")
+        const Products = await Response.json()
+        return Products
+    } catch (error) {
+        alert(error)
+    }
 }
 
+//function displayProducts (Product) {
+//    document.getElementById("items").innerHTML  += `
+//    <a href="./product.html?id=${Product._id}">
+//        <article>
+//          <img src="${Product.imageUrl}" alt="${Product.altTxt}">
+//            <h3 class="productName">${Product.name}</h3>
+//            <p class="productDescription">${Product.description}</p>
+//        </article>
+//    </a>`
+//}
+
 function displayProducts (Product) {
-    document.getElementById("items").innerHTML  += `
-    <a href="./product.html?id=${Product._id}">
-        <article>
-            <img src="${Product.imageUrl}" alt="${Product.altTxt}">
-            <h3 class="productName">${Product.name}</h3>
-            <p class="productDescription">${Product.description}</p>
-        </article>
-    </a>`
+    
+    const elt_a = document.createElement("a")
+    elt_a.setAttribute("href", "./product.html?id="+Product._id)
+
+    const elt_article = document.createElement("article")
+    elt_a.appendChild(elt_article)
+
+    const elt_img = document.createElement("img")
+    elt_img.setAttribute("src", Product.imageUrl)
+    elt_img.setAttribute("alt", Product.altTxt)
+    elt_article.appendChild(elt_img)
+
+    const elt_h3 = document.createElement("h3")
+    elt_h3.setAttribute("class", "productName")
+    elt_h3.textContent = Product.name
+    elt_article.appendChild(elt_h3)
+
+    const elt_p = document.createElement("p")
+    elt_p.setAttribute("class", "productDescription")
+    elt_p.textContent = Product.description
+    elt_article.appendChild(elt_p)
+
+    document.getElementById("items").appendChild(elt_a)
 }
